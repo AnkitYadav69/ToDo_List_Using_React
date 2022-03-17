a74473ecda42ad58e88deaa1df11a719b6ae07c4
AnkitYadav69 <ankit.yadav@celebaltech.com>
2022-03-17 23:00:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import react , { useState } from "react"
+import "./App.css"
+import ToDOList from "./ToDoList"
+
+const App = () =>{
+    const [itemName , setItemName] = useState("")
+    const [listOfItems , setItemList] = useState([])
+
+    const itemGetter = (event) =>{
+        setItemName(event.target.value)
+    }
+
+    const listAdder = () =>{
+        if(itemName != ""){
+            setItemList((items) => {
+                return [...items , itemName]
+            })
+            setItemName("")
+        }
+    }
+
+    const deleteItems = (id) =>{
+        setItemList((theList) => {
+            return theList.filter((element , index) =>{
+                return index !== id
+            })
+        })
+    }
+
+
+    return(
+        <div className = "container">
+            <div className = "to_do_box">
+                <br/>
+                <h1>ToDo List</h1>
+                <br/>
+                <input type = "text" placeholder = "Add task" onChange = {itemGetter} value = {itemName}></input>
+                <button className = "Adder" onClick = {listAdder}> ✔ </button>
+                <br/>
+                <br/>
+            <ol>
+                {
+                    listOfItems.map((value , id) =>
+                    {
+                        return <ToDOList 
+                        key={id}
+                        item = {value}
+                        index = {id}
+                        delete = {deleteItems}
+                        ></ToDOList>
+                    }
+                    )
+                }
+                {/* {console.log(itemName)}
+                {console.log(listOfItems)} */}
+            </ol>
+            </div>
+        </div>
+
+    )
+}
+
+export default App
